23ef1ce22bbcaad3d45b028943e75f846ff37eea
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-13 00:20:28 -0500

    d added @type, @spec

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -1,13 +1,17 @@
 defmodule Maybe do
   @behaviour Monad
 
+  @type t :: {:just, any} | :nothing
+
   def atoms(), do: [:just, :nothing]
 
+  @spec return(any) :: t
   def return(:nil),       do: :nothing
   def return(:nothing),   do: :nothing
   def return({:just, v}), do: {:just, v}
   def return(v),          do: {:just, v}
 
+  @spec bind(t, (any -> t)) :: t
   def bind({:just, v}, f) when is_function(f), do: f.(v)
   def bind(:nothing, f)   when is_function(f), do: :nothing
 end
