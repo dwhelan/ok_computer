6036f2878b0031e2933ea4c24831dae45e815929
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-12 23:33:14 -0500

    f added Maybe.bind

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -7,6 +7,8 @@ defmodule Maybe do
   def return(:nothing),   do: :nothing
   def return({:just, v}), do: {:just, v}
   def return(v),          do: {:just, v}
+
+  def bind({:just, v}, f) when is_function(f), do: f.(v)
 end
 
 defmodule MaybeTest do
@@ -23,12 +25,18 @@ defmodule MaybeTest do
       assert return(:nothing) == :nothing
     end
 
+    test "{:just, v} -> {:just, v}" do
+      assert return({:just, "v"}) == {:just, "v"}
+    end
+
     test "v -> {:just, v}" do
       assert return("v") == {:just, "v"}
     end
+  end
 
-    test "{:just, v} -> {:just, v}" do
-      assert return({:just, "v"}) == {:just, "v"}
+  describe "bind should" do
+    test "apply function if a just is provided" do
+      assert bind({:just, 1}, fn x -> x + 2 end) == 3
     end
   end
 end
