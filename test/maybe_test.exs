5a4816993ed2828468e0ea795600bc01b35231ce
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-14 00:43:33 -0500

    t added monad law tests

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -1,33 +1,44 @@
+defmodule Monad.Laws do
+  defmacro __using__ _ do
+    # Copied from Hex monad library: need to validate
+    quote do
+      test "left identity" do
+        f = fn x -> x * x end
+        a = 2
+        assert bind(return(a), f) == f.(a)
+      end
+
+      test "right identity" do
+        m = return 42
+        assert bind(m, &return/1) == m
+      end
+
+      test "associativity" do
+        f = fn (x) -> x * x end
+        g = fn (x) -> x - 1 end
+        m = return 2
+        assert bind(return(bind(m, f)), g) == bind(m, &bind(return(f.(&1)), g))
+      end
+    end
+  end
+end
+
 defmodule MaybeTest do
   use ExUnit.Case
 
   import Maybe
 
-  describe "return" do
-    test "nil -> :nothing" do
-      assert return(nil) == :nothing
-    end
+  use Monad.Laws
 
-    test ":nothing -> :nothing" do
-      assert return(:nothing) == :nothing
-    end
-
-    test "{:just, v} -> {:just, v}" do
-      assert return({:just, "v"}) == {:just, "v"}
-    end
-
-    test "v -> {:just, v}" do
-      assert return("v") == {:just, "v"}
-    end
+  test "return" do
+    assert return(nil)          == :nothing
+    assert return(:nothing)     == :nothing
+    assert return({:just, "v"}) == {:just, "v"}
+    assert return("v")          == {:just, "v"}
   end
 
-  describe "bind should" do
-    test "apply function if a just is provided" do
-      assert bind({:just, 1}, fn x -> {:just, x + 1} end) == {:just, 2}
-    end
-
-    test "bypass function if a nothing is provided" do
-      assert bind(:nothing, fn x -> {:just, x + 1} end) == :nothing
-    end
+  test "bind" do
+    assert bind({:just, "v"}, fn x -> {:just, "V"} end) == {:just, "V"}
+    assert bind(:nothing,     fn x -> {:just, "V"} end) == :nothing
   end
 end
