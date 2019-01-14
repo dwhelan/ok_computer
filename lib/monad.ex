b67bb7daf53c76e2f292bb698895b1a620795710
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-14 00:04:59 -0500

    r removed 'use Monad'

--- lib/monad.ex
+++ lib/monad.ex
@@ -4,38 +4,4 @@ defmodule Monad do
 
   @callback return(any) :: monad
   @callback bind(t, (any -> monad)) :: monad
-
-  defmacro __using__(modules) when is_list(modules) do
-    build_monad modules
-  end
-
-  defmacro __using__ module do
-    build_monad [module]
-  end
-
-  defp build_monad(modules) do
-    ensure_modules_are_monads modules
-    delegate_return hd modules
-  end
-
-  defp ensure_modules_are_monads [] do
-    raise ArgumentError, """
-    "use Monad" expects to be passed a module, or list of modules, with monad behaviour. For example:
-
-
-      defmodule MyModule do
-        use Monad, Maybe
-      end
-    """
-  end
-
-  defp ensure_modules_are_monads modules do
-    modules
-  end
-
-  defp delegate_return module do
-    quote do
-      import unquote(module)
-    end
-  end
 end
