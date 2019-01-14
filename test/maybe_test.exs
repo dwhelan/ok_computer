9fcc842017dfa2feb8b8c834d3f88b87e224854a
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-14 00:52:35 -0500

    t moved Laws into test_helper

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -1,33 +1,9 @@
-defmodule Monad.Laws do
-  defmacro __using__ _ do
-    # Copied from Hex monad library: need to validate
-    quote do
-      test "left identity" do
-        f = fn x -> x * x end
-        a = 2
-        assert bind(return(a), f) == f.(a)
-      end
-
-      test "right identity" do
-        m = return 42
-        assert bind(m, &return/1) == m
-      end
-
-      test "associativity" do
-        f = fn (x) -> x * x end
-        g = fn (x) -> x - 1 end
-        m = return 2
-        assert bind(return(bind(m, f)), g) == bind(m, &bind(return(f.(&1)), g))
-      end
-    end
-  end
-end
-
 defmodule MaybeTest do
   use ExUnit.Case
 
   import Maybe
 
+  require Monad.Laws
   use Monad.Laws
 
   test "return" do
@@ -38,7 +14,7 @@ defmodule MaybeTest do
   end
 
   test "bind" do
-    assert bind({:just, "v"}, fn x -> {:just, "V"} end) == {:just, "V"}
-    assert bind(:nothing,     fn x -> {:just, "V"} end) == :nothing
+    assert bind({:just, "v"}, fn _ -> {:just, "V"} end) == {:just, "V"}
+    assert bind(:nothing,     fn _ -> {:just, "V"} end) == :nothing
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,26 @@
 ExUnit.start()
 
-defmodule TestHelper do
-  def assert_code_error code do
-    ExUnit.Assertions.assert_raise ArgumentError, fn -> Code.eval_string(code) end
+defmodule Monad.Laws do
+  defmacro __using__ _ do
+    # Copied from Hex monad library: need to validate
+    quote do
+      test "left identity" do
+        f = fn x -> x * x end
+        a = 2
+        assert bind(return(a), f) == f.(a)
+      end
+
+      test "right identity" do
+        m = return 42
+        assert bind(m, &return/1) == m
+      end
+
+      test "associativity" do
+        f = fn (x) -> x * x end
+        g = fn (x) -> x - 1 end
+        m = return 2
+        assert bind(return(bind(m, f)), g) == bind(m, &bind(return(f.(&1)), g))
+      end
+    end
   end
 end
