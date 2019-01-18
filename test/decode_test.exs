e297e3ddbf1f79e661e9ab9371bd3466bfb61603
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-18 13:33:32 -0500

    t added checks for errors

--- test/decode_test.exs
+++ test/decode_test.exs
@@ -4,33 +4,46 @@ defmodule EncodeTest do
 
   use Monad.Laws
 
-  defmodule TestCodec do
+  defmodule OkEncoder do
     import OkError
 
     def encode value do
-      ok {"encode(#{value})", "bytes", TestCodec}
+      ok {"encode(#{value})", "bytes", OkEncoder}
+    end
+  end
+
+  defmodule ErrorEncoder do
+    import OkError
+
+    def encode _value do
+      error "encode error"
     end
   end
 
   test "return" do
     assert Encode.return({:error, "reason"})                == error "reason"
-    assert Encode.return({:ok, {"value", <<>>, TestCodec}}) == ok {"value", <<>>, TestCodec}
+    assert Encode.return({:ok, {"value", <<>>, OkEncoder}}) == ok {"value", <<>>, OkCode}
   end
 
   describe "bind/2 with" do
-    test "an 'ok' result from 'map' should encode the mapped value" do
-      map = fn {value, bytes, codec} -> ok {"f(#{value})", bytes, codec} end
-      assert Encode.bind({:ok, {"value", <<>>, TestCodec}}, map) == ok {"encode(f(value))", "bytes", TestCodec}
+    test "an 'ok' from 'map' should encode the mapped value" do
+      map = fn {value, bytes, codec} -> ok {"map(#{value})", bytes, codec} end
+      assert Encode.bind({:ok, {"value", <<>>, OkEncoder}}, map) == ok {"encode(map(value))", "bytes", OkCode}
     end
 
-    test "an 'ok' result from an identity 'map' should encode the original value" do
+    test "an 'ok' from an identity 'map' should encode the original value" do
       map = fn result -> ok result end
-      assert Encode.bind({:ok, {"value", <<>>, TestCodec}}, map) == ok {"encode(value)", "bytes", TestCodec}
+      assert Encode.bind({:ok, {"value", <<>>, OkEncoder}}, map) == ok {"encode(value)", "bytes", OkCode}
     end
 
-    test "an 'error' result from 'map' should return the error" do
-      map = fn result -> error "reason" end
-      assert Encode.bind({:ok, {"value", <<>>, TestCodec}}, map) == error "reason"
+    test "an 'error' from 'map' should return the map error" do
+      map = fn _ -> error "map error" end
+      assert Encode.bind({:ok, {"value", <<>>, OkEncoder}}, map) == error "map error"
+    end
+
+    test "an 'error' from 'encode' should return the encode error" do
+      map = fn result -> ok result end
+      assert Encode.bind({:ok, {"value", <<>>, ErrorEncoder}}, map) == error "encode error"
     end
 
     test "bind error {value, bytes, codec}, f" do
