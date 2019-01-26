bfd702ac54981e5b7148f57e3aae76617312e26f
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-26 17:50:36 -0500

    r removed OkError behaviour from Encode

--- lib/encode.ex
+++ lib/encode.ex
@@ -1,34 +1,8 @@
 defmodule Encode do
-  @behaviour Monad
-
-  @type codec    :: module
-  @type encoding :: {binary, codec, any}
-  @type result   :: {:ok, encoding} | {:error, any}
-
-  @spec return(result) :: result
-  defdelegate return(value), to: OkError
-
-  @spec bind(result, (any -> result)) :: result
-  def bind({:ok, value}, map) when is_function(map) do
-    with {:ok, {_, codec, value}} <- map.(value),
-         {:ok, bytes}             <- apply(codec, :encode, [value]) do
-      {:ok, {bytes, codec, value}}
-    else
-      error -> error
-    end
-  end
-
-  def bind({:error, reason}, map) when is_function(map) do
-    {:error, reason}
-  end
-
-  @spec ok(encoding) :: result
-  def ok({bytes, codec, value}) when is_binary(bytes) and is_atom(codec) do
-    {:ok, {bytes, codec, value}}
-  end
+  require OkError
 
   def map {:ok, value}, f do
-    value |> f.() |> return
+    value |> f.() |> OkError.return
   end
 
   def map {:error, reason}, _f do
--- test/encode_test.exs
+++ test/encode_test.exs
@@ -24,52 +24,6 @@ defmodule EncodeTest do
     end
   end
 
-  describe "return/1 with" do
-    test "an error" do
-      assert Encode.return({:error, "reason"}) == error "reason"
-    end
-
-    test "an ok" do
-      assert Encode.return({:ok, {<<>>, OkEncoder, "value"}}) == ok {<<>>, OkEncoder, "value"}
-    end
-  end
-
-  describe "bind/2 with" do
-    test "an 'ok' from 'map' should encode the mapped value" do
-      map = fn {bytes, codec, value} -> ok {bytes, codec, "map(#{value})"} end
-      assert Encode.bind({:ok, {<<>>, OkEncoder, "value"}}, map) == ok {<<"bytes">>, OkEncoder, "map(value)"}
-    end
-
-    test "an input 'error' should return the input error" do
-      map = fn result -> ok result end
-      assert Encode.bind({:error, "reason"}, map) == error "reason"
-    end
-
-    test "an 'error' from 'map' should return the map error" do
-      map = fn _ -> error "map error" end
-      assert Encode.bind({:ok, {<<>>, OkEncoder, "value"}}, map) == error "map error"
-    end
-
-    test "an 'error' from 'encode' should return the encode error" do
-      map = fn result -> ok result end
-      assert Encode.bind({:ok, {<<>>, ErrorEncoder, "value"}}, map) == error "encode error"
-    end
-  end
-
-  describe "ok/1 with" do
-    test "a valid result should return the result in an ok tuple" do
-      assert Encode.ok({<<>>, OkEncoder, "value"}) == {:ok, {<<>>, OkEncoder, "value"}}
-    end
-
-    test "non-binary bytes should raise a FunctionClauseError" do
-      assert_code_raise FunctionClauseError, ~s(Encode.ok {:not_binary, OkEncoder, "value"})
-    end
-
-    test "a non-atom codec should raise a FunctionClauseError" do
-      assert_code_raise FunctionClauseError, ~s(Encode.ok {<<>>, "not_a_module", "value"})
-    end
-  end
-
   describe "map/2 with" do
     test "an ok result should map the value" do
       map = fn value -> String.upcase(value) end
