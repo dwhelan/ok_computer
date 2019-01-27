a6eea4369b84af54167b05684619cf8a2ecdfa0f
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-27 00:53:19 -0500

    f added Byte.encode

--- test/byte_test.exs
+++ test/byte_test.exs
@@ -7,6 +7,16 @@ defmodule Byte.Decode do
 
 end
 
+defmodule Byte.Encode do
+  def encode(value) when is_integer(value) and value >= 0 and value <= 255 do
+    {:ok, <<value>>}
+  end
+
+  def encode value do
+    {:error, {:invalid_byte, value}}
+  end
+end
+
 defmodule Byte.DecodeTest do
   use ExUnit.Case
 
@@ -18,5 +28,19 @@ defmodule Byte.DecodeTest do
 
   test "decode when bytes available" do
     assert decode(<<42>>) == {:ok, {42, <<>>}}
+  end
+end
+
+defmodule Byte.EncodeTest do
+  use ExUnit.Case
+
+  import Byte.Encode
+
+  test "encode with a byte value" do
+    assert encode(42) == {:ok, <<42>>}
+  end
+
+  test "encode with a non-byte value" do
+    assert encode(256) == {:error, {:invalid_byte, 256}}
   end
 end
