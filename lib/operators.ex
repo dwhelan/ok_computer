ce2b1eef9222240c9555204b0f8ee9e38c0cde73
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-30 21:28:30 -0500

    r moved ~> operator to Operators module

---
+++ lib/operators.ex
@@ -0,0 +1,13 @@
+defmodule Operators do
+  defmacro a ~> f  do
+    pipe_bind a, f
+  end
+
+  defp pipe_bind(a, f = {atom, _, _}) when atom in [:fn, :&] do
+    quote location: :keep, do: unquote(a) |> bind(unquote f) |> return
+  end
+
+  defp pipe_bind a, f do
+    quote location: :keep, do: unquote(a) |> bind(&unquote(f)/1) |> return
+  end
+end
