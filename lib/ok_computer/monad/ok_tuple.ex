28c6883a74c25558162541586bcb7f1fc7d29ae0
Declan Whelan <dwhelan@dpwhelan.com>
2019-09-29 15:29:14 -0400

    add OkTuple and ErrorTuple

---
+++ lib/ok_computer/monad/ok_tuple.ex
@@ -0,0 +1,25 @@
+defmodule OkComputer.Monad.OkTuple do
+  alias OkComputer.Monad
+  @behaviour Monad
+
+  @impl Monad
+  def return({:ok, a}), do: {:ok, a}
+  def return(a), do: {:ok, a}
+
+  @impl Monad
+  def bind({:ok, a}, f), do: f.(a)
+  def bind(a, _f), do: a
+end
+
+defmodule OkComputer.Monad.ErrorTuple do
+  alias OkComputer.Monad
+  @behaviour Monad
+
+  @impl Monad
+  def return({:error, a}), do: {:error, a}
+  def return(a), do: {:error, a}
+
+  @impl Monad
+  def bind({:error, a}, f), do: f.(a)
+  def bind(a, _f), do: a
+end
---
+++ test/ok_computer/monad/ok_tuple_test.exs
@@ -0,0 +1,41 @@
+defmodule OkComputer.Monad.OkTupleTest do
+  use ExUnit.Case
+  import Monad.Laws
+  alias OkComputer.Monad.OkTuple
+  import OkTuple
+
+  test "return" do
+    assert return({:ok, :a}) == {:ok, :a}
+    assert return(:a) == {:ok, :a}
+  end
+
+  @tag :skip
+  test "bind" do
+    assert bind(nil, &to_string/1) == nil
+    assert bind(false, &to_string/1) == false
+    assert bind(:anything_else, &to_string/1) == "anything_else"
+  end
+
+#  test_monad(OkTuple, {":ok, a"})
+end
+
+defmodule OkComputer.Monad.ErrorTupleTest do
+  use ExUnit.Case
+  import Monad.Laws
+  alias OkComputer.Monad.ErrorTuple
+  import ErrorTuple
+
+  test "return" do
+    assert return({:error, :a}) == {:error, :a}
+    assert return(:a) == {:error, :a}
+  end
+
+  @tag :skip
+  test "bind" do
+    assert bind(nil, &to_string/1) == nil
+    assert bind(false, &to_string/1) == false
+    assert bind(:anything_else, &to_string/1) == "anything_else"
+  end
+
+#  test_monad(OkTuple, :anything_truthy)
+end
