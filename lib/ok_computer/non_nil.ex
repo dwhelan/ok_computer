b60cc629acb9f07d4217602a3e480b3e385c6614
Declan Whelan <dwhelan@dpwhelan.com>
2019-09-30 01:13:35 -0400

    add typespecs to Case

--- lib/ok_computer/non_nil.ex
+++ lib/ok_computer/non_nil.ex
@@ -2,5 +2,6 @@ defmodule OkComputer.NonNil do
   import OkComputer.Switch
   alias OkComputer.Monad.{NonNil, Nil}
 
+  @spec monadic_switch(nil, nil) :: Macro.t()
   monadic_switch(NonNil, Nil)
 end
