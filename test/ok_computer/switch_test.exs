6459e14920c9ed41a72a99bc181c9cef674c3d77
Declan Whelan <dwhelan@dpwhelan.com>
2019-10-05 18:44:25 -0400

    add more Switch tests

--- test/ok_computer/switch_test.exs
+++ test/ok_computer/switch_test.exs
@@ -40,3 +40,37 @@ defmodule OkComputer.SwitchOperatorTest do
     )
   end
 end
+
+defmodule OkComputer.Switch.PipeAndOperatorTest do
+  alias OkComputer.Monad.{Value, Nil}
+  alias OkComputer.Operation.Case
+
+  use ExUnit.Case
+  import OkComputer.Switch
+
+  build [Case], ~>: Value, ~>>: Nil
+
+  test "value pipe" do
+    assert :value ~> to_string() == "value"
+  end
+
+  test "nil pipe" do
+    assert nil ~>> to_string() == ""
+  end
+
+  test "case_value" do
+    assert(
+      case_value :value do
+        value -> to_string(value)
+      end == "value"
+    )
+  end
+
+  test "case_nil" do
+    assert(
+      case_nil nil do
+        value -> to_string(value)
+      end == ""
+    )
+  end
+end
