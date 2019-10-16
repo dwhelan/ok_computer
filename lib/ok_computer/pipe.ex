e522b6750516386b1e3b4928706cdaacf8e60036
Declan Whelan <dwhelan@dpwhelan.com>
2019-10-16 01:17:09 -0400

    improve Pipe docs and typespecs

--- lib/ok_computer/pipe.ex
+++ lib/ok_computer/pipe.ex
@@ -1,13 +1,28 @@
 defmodule OkComputer.Pipe do
   @moduledoc """
-  Describes pipe behaviour.
+  A monad that short circuits inputs it cannot handle.
+
+  `OkComputer.Monad.bind/2` and `OkComputer.Monad.fmap/2` expect to be given a monad as the input.
+  Behaviour when they are given an input they can't handle is undefined as it is out of scope for a type-based monad.
+
+  A PipeMonad implements `bind/2` and `fmap/2` so they always return inputs they cannot handle.
+
+  This lets you pipe functions that operate in different monadic contexts together.
   """
+  @type monad :: any
+  @type a :: any
 
-  @doc "fmap"
-  @callback fmap(t :: term, (term -> term)) :: t :: term
+  @doc """
+  Calls
 
-  @doc "bind"
-  @callback bind(t :: term, (term -> t :: term)) :: t :: term
+  A default implementation simply calls `bind/2`
+  """
+  @callback fmap(monad, f :: (a -> a)) :: monad
+
+  @doc """
+
+  """
+  @callback bind(monad, f :: (a -> monad)) :: monad
 
   defmacro __using__(_) do
     quote do
