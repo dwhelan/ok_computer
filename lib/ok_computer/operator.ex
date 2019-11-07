ec41a03495e09ddee60e3217852508a3612d7f4c
Declan Whelan <dwhelan@dpwhelan.com>
2019-11-07 00:39:48 -0500

    create general operators (as a sidecar)

--- lib/ok_computer/operator.ex
+++ lib/ok_computer/operator.ex
@@ -16,25 +16,84 @@ defmodule OkComputer.Operator do
 
   def create(atom, f, create_function) do
     operator = Module.concat(__MODULE__, Map.get(@operators, atom))
-    operator_arity = operator_arity(atom)
-    arity = function_arity(f)
+    operator_arities = operator_arities(atom)
+    arity = arity(f)
 
-    if arity in operator_arity do
+    if arity in operator_arities do
       apply(operator, create_function, [f, arity])
     else
-      raise "expected a function with arity in #{inspect(operator_arity)}, but got a function with arity #{
+      raise "expected a function with arity in #{inspect(operator_arities)}, but got a function with arity #{
               arity
             }"
     end
   end
 
-  defp function_arity(f) do
+  defmacro operator2(atom, f) do
+    create2(atom, f, :def)
+  end
+
+  defmacro operator_macro2(atom, f) do
+    create2(atom, f, :defmacro)
+  end
+
+  def create2(atom, f, type) do
+    if arity(f) in operator_arities(atom) do
+      operator(atom, f, arity(f), type)
+    else
+      raise "expected a function with arity in #{inspect(operator_arities(atom))}, but got a function with arity #{
+              arity(f)
+            }"
+    end
+  end
+
+  def operator(atom, f, 1, :def) do
+    quote do
+      import Kernel, except: [{unquote(atom), 1}]
+
+      def unquote(atom)(input) do
+        unquote(f).(input)
+      end
+    end
+  end
+
+  def operator(atom, f, 2, :def) do
+    quote do
+      import Kernel, except: [{unquote(atom), 2}]
+
+      def unquote(atom)(left, right) do
+        unquote(f).(left, right)
+      end
+    end
+  end
+
+  def operator(atom, f, 1, :defmacro) do
+    quote do
+      import Kernel, except: [{unquote(atom), 1}]
+
+      defmacro unquote(atom)(input) do
+        unquote(f).(input)
+      end
+    end
+  end
+
+  def operator(atom, f, 2, :defmacro) do
+    quote do
+      import Kernel, except: [{unquote(atom), 2}]
+
+      defmacro unquote(atom)(left, right) do
+        unquote(f).(left, right)
+      end
+    end
+  end
+
+
+  defp arity(f) do
     {f, _} = Code.eval_quoted(f)
     {:arity, arity} = Function.info(f, :arity)
     arity
   end
 
-  def operator_arity(atom) do
+  def operator_arities(atom) do
     [1, 2] |> Enum.filter(fn arity -> Macro.operator?(atom, arity) end)
   end
 end
