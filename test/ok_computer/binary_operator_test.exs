40dfc617beba9cfa41defef1540cc219106cb78e
Declan Whelan <dwhelan@dpwhelan.com>
2019-11-09 16:47:24 -0500

    start tests for each binary operator

--- test/ok_computer/binary_operator_test.exs
+++ test/ok_computer/binary_operator_test.exs
@@ -2,9 +2,32 @@ defmodule OkComputer.BinaryOperatorTest do
   use ExUnit.Case
   import OkComputer.Operator
 
-  operator :+, fn left, right -> to_string(left) <> to_string(right) end
 
-  test "+/2" do
-    assert :a + :b == "ab"
+  # Should raise error
+  operator :., fn left, right -> "#{left}#{right}" end
+
+  operator :*, fn left, right -> "#{left}#{right}" end
+  operator :/, fn left, right -> "#{left}#{right}" end
+  operator :+, fn left, right -> "#{left}#{right}" end
+  operator :-, fn left, right -> "#{left}#{right}" end
+  operator :++, fn left, right -> "#{left}#{right}" end
+  operator :--, fn left, right -> "#{left}#{right}" end
+  operator :.., fn left, right -> "#{left}#{right}" end
+  operator :<>, fn left, right -> "#{left}#{right}"  end
+
+#  test "./2", do: assert(.(:a, :b) == "ab")
+  test "*", do: assert(:a * :b == "ab")
+  test "/", do: assert(:a / :b == "ab")
+  test "+/2", do: assert(:a + :b == "ab")
+  test "-/2", do: assert(:a - :b == "ab")
+  test "++", do: assert(:a ++ :b == "ab")
+  test "--", do: assert(:a -- :b == "ab")
+  test "..", do: assert(:a .. :b == "ab")
+  test "<>", do: assert(:a <> :b == "ab")
+
+  test "./2" do
+    assert_raise SyntaxError, ~r"syntax error before: b", fn ->
+      Code.eval_string(":a . :b")
+    end
   end
 end
--- test/ok_computer/unary_operator_test.exs
+++ test/ok_computer/unary_operator_test.exs
@@ -9,14 +9,16 @@ defmodule OkComputer.UnaryOperatorTest do
   operator :not, fn input -> to_string(input) end
   operator :~~~, fn input -> to_string(input) end
 
-  test "@/1", do: assert(@:a == "a")
+  test "@", do: assert(@:a == "a")
   test "+/1", do: assert(+:a == "a")
   test "-/1", do: assert(-:a == "a")
-  test "!/1", do: assert(!:a == "a")
-  test "not/1", do: assert(not :a == "a")
-  test "~~~/1", do: assert(~~~:a == "a")
+  test "!", do: assert(!:a == "a")
+  test "not", do: assert(not :a == "a")
+  test "~~~", do: assert(~~~:a == "a")
 
-  assert_raise CompileError, ~r"cannot use \^a outside of match clauses", fn ->
-    Code.eval_string("^a")
+  test "^/1" do
+    assert_raise CompileError, ~r"cannot use \^a outside of match clauses", fn ->
+      Code.eval_string("^a")
+    end
   end
 end
