1d1ebec347d4efd27dafc209903b785d5668cd6d
Declan Whelan <dwhelan@dpwhelan.com>
2019-11-11 01:37:36 -0500

    test Operator errors through create/3

--- test/ok_computer/operator_test.exs
+++ test/ok_computer/operator_test.exs
@@ -1,56 +1,42 @@
 defmodule OkComputer.OperatorTest do
   use ExUnit.Case
+  import OkComputer.Operator
 
   def assert_operator_error_raise(string) do
     assert_raise(OkComputer.OperatorError, fn -> Code.eval_string(string) end)
   end
 
   describe "unary operators" do
-    test "can't use '^'" do
-      assert_operator_error_raise(~S"""
-      defmodule OkComputer.BadOperator do
-        import OkComputer.Operator
-        operator :^, fn input -> input end
-      end
-      """)
+    test "can't use ^" do
+      assert_raise(OkComputer.OperatorError, fn ->
+        create(:def, :^, quote(do: &to_string/1))
+      end)
     end
   end
 
   describe "binary operators" do
-    test "can't use '.'" do
-      assert_operator_error_raise(~S"""
-      defmodule OkComputer.BadOperator do
-        import OkComputer.Operator
-        operator :".", fn left, right -> "#{left}#{right}"  end
-      end
-      """)
+    test "can't use ." do
+      assert_raise(OkComputer.OperatorError, fn ->
+        create(:def, :., quote(do: &apply/2))
+      end)
     end
 
-    test "can't use 'not in'" do
-      assert_operator_error_raise(~S"""
-      defmodule OkComputer.BadOperator do
-        import OkComputer.Operator
-        operator :"not in", fn left, right -> "#{left}#{right}"  end
-      end
-      """)
+    test "can't use not in" do
+      assert_raise(OkComputer.OperatorError, fn ->
+        create(:def, :"not in", quote(do: &apply/2))
+      end)
     end
 
-    test "can't use '=>'" do
-      assert_operator_error_raise(~S"""
-      defmodule OkComputer.BadOperator do
-        import OkComputer.Operator
-        operator :"=>", fn left, right -> "#{left}#{right}"  end
-      end
-      """)
+    test "can't use =>" do
+      assert_raise(OkComputer.OperatorError, fn ->
+        create(:def, :"=>", quote(do: &apply/2))
+      end)
     end
 
-    test "can't use 'when'" do
-      assert_operator_error_raise(~S"""
-      defmodule OkComputer.BadOperator do
-        import OkComputer.Operator
-        operator :when, fn left, right -> "#{left}#{right}"  end
-      end
-      """)
+    test "can't use when" do
+      assert_raise(OkComputer.OperatorError, fn ->
+        create(:def, :when, quote(do: &apply/2))
+      end)
     end
   end
 end
