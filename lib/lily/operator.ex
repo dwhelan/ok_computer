456eb57f00438dc7539f133ef8007014cdeba379
Declan Whelan <dwhelan@dpwhelan.com>
2019-11-18 00:45:08 -0500

    add Operator.arity docs

--- lib/lily/operator.ex
+++ lib/lily/operator.ex
@@ -272,6 +272,16 @@ defmodule Lily.Operator do
     end
   end
 
+  @doc """
+  Returns the arity of a quoted function.
+
+  ## Examples
+
+      iex> arity quote(do: fn _ -> nil end), __ENV__
+      1
+      iex> arity quote(do: fn _, _ -> nil end), __ENV__
+      2
+  """
   @spec arity(Macro.t(), Macro.Env.t()) :: integer
   def arity(f, env) do
     {f, _} = Code.eval_quoted(f, [], env)
--- test/lily/operator_test.exs
+++ test/lily/operator_test.exs
@@ -3,6 +3,29 @@ defmodule Lily.OperatorTest do
   alias Lily.{Operator, Error}
   import Operator
 
+  defoperators(
+    -: fn a -> "#{a}" end,
+    +: &"#{&1}",
+    @: &to_string/1,
+    !: &Kernel.to_string/1,
+    ~>: fn a, b -> "#{a}#{b}" end,
+    ~>>: &"#{&1}#{&2}",
+    <~: &__MODULE__.to_string/2
+  )
+
+  describe "defoperators: unary operator with" do
+    test "anonymous function", do: assert(-:a == "a")
+    test "capture operator", do: assert(+:a == "a")
+    test "local capture", do: assert(@:a == "a")
+    test "remote capture", do: assert(!:a == "a")
+  end
+
+  describe "defoperators binary operator with" do
+    test "anonymous function", do: assert(:a ~> :b == "ab")
+    test "capture operator", do: assert(:a ~>> :b == "ab")
+    test "remote capture", do: assert(:a <~ :b == "ab")
+  end
+
   describe "create/3" do
     test "raise if type is not :def or :defmacro" do
       assert_raise(
@@ -31,30 +54,24 @@ defmodule Lily.OperatorTest do
     end
   end
 
+  describe "arity/2" do
+    test "raise if f is an invalid quoted expression" do
+      assert_raise(
+        CompileError,
+        ~r/invalid quoted expression/,
+        fn -> arity({:invalid_quoted_expression}, __ENV__) end
+      )
+    end
+
+    test "raise if quoted is not a function" do
+      assert_raise(
+        ArgumentError,
+        fn -> arity(quote(do: :not_a_function), __ENV__) end
+      )
+    end
+  end
+
   def to_string(a, b) do
     "#{a}#{b}"
-  end
-
-  defoperators(
-    -: fn a -> "#{a}" end,
-    +: &"#{&1}",
-    @: &to_string/1,
-    !: &Kernel.to_string/1,
-    ~>: fn a, b -> "#{a}#{b}" end,
-    ~>>: &"#{&1}#{&2}",
-    <~: &__MODULE__.to_string/2
-  )
-
-  describe "defoperators: unary operator with" do
-    test "anonymous function", do: assert(-:a == "a")
-    test "capture operator", do: assert(+:a == "a")
-    test "local capture", do: assert(@:a == "a")
-    test "remote capture", do: assert(!:a == "a")
-  end
-
-  describe "defoperators binary operator with" do
-    test "anonymous function", do: assert(:a ~> :b == "ab")
-    test "capture operator", do: assert(:a ~>> :b == "ab")
-    test "remote capture", do: assert(:a <~ :b == "ab")
   end
 end
